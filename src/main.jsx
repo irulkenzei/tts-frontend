a73b331077ef95cb6a91aaa7194dc6261c6f9ae8
Chairul Anwar <chairulanwar2104@gmail.com>
2026-07-14 23:59:50 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import AdminListings from './AdminListings.jsx'
 import PublicVoiceListings from './PublicVoiceListings.jsx'
 import ResetPassword from './ResetPassword.jsx'
+import AdminDataManager from './AdminDataManager.jsx'
 const path = window.location.pathname
 const RootComponent =
   path === '/admin' ? AdminListings
   : path === '/voices' ? PublicVoiceListings
   : path === '/reset-password' ? ResetPassword
+  : path === '/admin-emotions' ? AdminDataManager
   : App
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
