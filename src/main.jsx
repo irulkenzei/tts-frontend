4647b821db4315c3114660f2792eb8f26f344ad0
Chairul Anwar <chairulanwar2104@gmail.com>
2026-07-16 06:59:36 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import App from './App.jsx';
-import AdminListings from './AdminListings.jsx';
-import PublicVoiceListings from './PublicVoiceListings.jsx';
-import ResetPassword from './ResetPassword.jsx';
-import AdminDataManager from './AdminDataManager.jsx'; // Ini untuk admin-emotions
-import ProtectedRoute from './components/ProtectedRoute'; 
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import AdminListings from './AdminListings.jsx'
+import PublicVoiceListings from './PublicVoiceListings.jsx'
+import ResetPassword from './ResetPassword.jsx'
+import AdminDataManager from './AdminDataManager.jsx'
+const path = window.location.pathname
+const RootComponent =
+  path === '/admin' ? AdminListings
+  : path === '/voices' ? PublicVoiceListings
+  : path === '/reset-password' ? ResetPassword
+  : path === '/admin-emotions' ? AdminDataManager
+  : App
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* Rute Publik: Bebas diakses siapa saja */}
-        <Route path="/" element={<App />} />
-        <Route path="/admin" element={<AdminListings />} />
-        <Route path="/voices" element={<PublicVoiceListings />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-
-        {/* Rute Terlindungi (Protected): Harus login/melewati pengecekan */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/admin-emotions" element={<AdminDataManager />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+  <React.StrictMode>
+    <RootComponent />
+  </React.StrictMode>,
+)
