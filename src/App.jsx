4b2865bc5df1264eb0cc1f1ef63cb387327add7d
Chairul Anwar <chairulanwar2104@gmail.com>
2026-07-20 21:53:10 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1561,6 +1561,7 @@ const TtsServer = () => {
           <a href="/request-feature">Request a Feature</a>
           <a href="/privacy-policy">Privacy Policy</a>
           <a href="/terms-of-service">Terms</a>
+          <a href='/delete-account'>Delete Account</a>
         </nav>
       </footer>
 
